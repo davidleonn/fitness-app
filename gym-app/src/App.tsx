8575324db7d56c0dfb259e69bc4052956296e820
david <davidleon_06@hotmail.com>
2023-03-21 17:57:30 +0100

    Homepage done

--- gym-app/src/App.tsx
+++ gym-app/src/App.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 
 import Navbar from "@/features/navbar";
 import { SelectedPage } from "./utils/types";
+import Home from "./features/home";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
@@ -19,6 +21,7 @@ function App() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
   return (
     <div className="app bg-gray-20">
       <Navbar
@@ -26,6 +29,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
