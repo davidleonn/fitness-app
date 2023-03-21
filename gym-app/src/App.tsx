c7d43ed6f395dab390b3af593ae9103f0e4409d5
david <davidleon_06@hotmail.com>
2023-03-21 14:57:21 +0100

    Added navbar with navigation prepared

--- gym-app/src/App.tsx
+++ gym-app/src/App.tsx
@@ -1,5 +1,14 @@
+import Navbar from "@/features/navbar";
+import { useState } from "react";
+
 function App() {
-  return <div className="App">App</div>;
+  const [selectedPage, setSelectedPage] = useState("home");
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
