698ff0ac444b270ace484264e58275df2f792b2d
david <davidleon_06@hotmail.com>
2023-03-22 15:55:03 +0100

    Finnished benefits page

--- gym-app/src/features/benefits/Benefit.tsx
+++ gym-app/src/features/benefits/Benefit.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 import { SelectedPage } from "@/utils/types";
+import { motion } from "framer-motion";
+
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
 
 type Props = {
   icon: JSX.Element;
@@ -12,7 +18,10 @@ type Props = {
 
 const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
+    <motion.div
+      variants={childVariant}
+      className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"
+    >
       <div className="mb-4 flex justify-center">
         <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
           {icon}
@@ -27,7 +36,7 @@ const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
       >
         <p>Learn More</p>
       </AnchorLink>
-    </div>
+    </motion.div>
   );
 };
 
